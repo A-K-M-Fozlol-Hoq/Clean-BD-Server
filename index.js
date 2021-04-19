fca7f843c2ef0cc84f9faed92836cdad422f80ba
A-K-M-Fozlol-Hoq <haqueakmfazlul001@gmail.com>
2021-04-19 23:53:29 +0600

    std

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfwri.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
